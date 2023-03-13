e0587009942c9f83abb2db61ba75b5c3f29badae
atin-sahu <jayshiv7171@gmail.com>
2023-03-13 09:27:43 +0530

    initial commit

---
+++ ravi.js
@@ -0,0 +1,44 @@
+
+const [faqsData, setFaqsDat] = useState([
+    { 
+        title: "Are there any trial sessions available?",
+        description: "<p>A complementary discovery session lasting an hour will take place during which the coach and coachee engage in conversation to uncover any potential professional or personal obstacles. In this session, the coachee will discuss their personal challenges, problems, and goals while also discussing how coaching might be beneficial.<br> You can leverage this session to analyse and understand the scope of improvement and proceed from there as suitable.</p>"
+    },
+    { 
+        title: "What are the general requirements for becoming a Nordsta coach?",
+        description: "<p>Nordsta is a coaching marketplace that aims to bring together compatible coaches and coachees who can together reimagine growth and create an impact.<br> To become a “Nordsta Certified Coach,” you must have a minimum of 15 years of substantial experience and keep certifications from ICF and ACC.<br> If you’ve got what it takes, <a href=''>sign up</a> here.</p>"
+    },
+    { 
+        title: "Where are the Nordsta coaches from, and what are their qualifications?",
+        description: "<p>At Nordsta, you will have access to a large pool of certified coaches who have the required expertise subject to the industry you belong to.<br> We ensure to only onboard the best-qualified coaches with a minimum experience of 15 years in the industry who pass to become thought leaders in their domain and hold certifications such as ICF & ACC.</p>"
+    },
+    { 
+        title: "How do I know I will get the help I need?",
+        description: "<p>Our coaches are skilled and experienced; they have much to pass on to their coachees. Except, it is entirely upon the individual to benefit and draw from the sessions as much as possible.<br> Moreover, you’ll be helped with growth-hacking customized strategies and action plans which ensure success on proper execution.</p>"
+    },
+    { 
+        title: "What payment structure do you follow?",
+        description: "<p>At Nordsta, you should get value for your money, which is why we promote the pay-as-you-go payment structure.<br> The most significant benefit is that no resources are wasted, and you only pay for what you consume.<br> Thus, ensuring growth reasonably.</p> "
+    },
+    { 
+        title: "Can I get my company to pay for this?",
+        description: "<p>You can always give it a try since most organizations today emphasize personal development and continual learning for their employees; mostly, they are open to reimbursing their employees for the effort they are putting in.<br> Nonetheless, the sessions are affordable, given the value they offer.<br> So don’t wait, join before it’s too late!</p>"
+    },
+    { 
+        title: "How is Nordsta different from others?",
+        description: "<p>Unlike other coaching marketplaces, Nordsta focuses on offering holistic development of individuals by analyzing the grey areas and offering them the opportunity to learn while experiencing. Collaborative & experiential learning are two significant ways Nordsta powers your transformation.</p>"
+    },
+    { 
+        title: "Will I get to network with other Nordsta members?",
+        description: "<p>Nordsta focuses on offering collaborative learning opportunities. Hence you will be allowed to work with people in your industry and learn together.<br> Certain functionalities, such as Nordsta’s in-built messenger, ensure no information siloes are created and coachees learn coherently.</p>"
+    },
+    { 
+        title: "Why do I need coaching?",
+        description: "<p>Coaching helps individuals better understand their core strengths and identity skills and provides executable strategies aligned with their goals, whether to find a new direction in their career or explore new realms of growth.<br> Whether you are looking to switch your current career path or are willing to break through corporate politics and find a solution to job crises and uncertainties, Nordsta coaches can be your ultimate savior.</p>"
+    },
+    { 
+        title: "What if I want to extend my coaching sessions?",
+        description: "<p>Extension of sessions is subject to the coachees’ progress. If both coach and coachee are of the opinion that further sessions are required then the sessions can be extended upon the realization of payment for additional sessions until the coachee feels confident in themselves.</p>"
+    }
+]);
+
